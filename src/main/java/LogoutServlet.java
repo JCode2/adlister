ca85dc1f70df85693ca66a53ae455aa40fff54a9
josh <josh.fierrosga@gmail.com>
2016-12-15 14:44:16 -0600

    MVC login servlet exercise

---
+++ src/main/java/LogoutServlet.java
@@ -0,0 +1,22 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by joshua on 12/15/16.
+ */
+@WebServlet(name = "LogoutServlet", urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getSession().removeAttribute("user");
+        request.getSession().invalidate();
+        response.sendRedirect("/login");
+    }
+}
--- src/main/webapp/WEB-INF/ads/index.jsp
+++ src/main/webapp/WEB-INF/ads/index.jsp
@@ -10,9 +10,9 @@
 <jsp:include page="/partials/navbar.jsp" />
 
 <div class="container">
-    <h1>Here Are all the ads!</h1>
+    <h1>Here are all the ads!</h1>
 
-    <c:forEach var="ad" items="${ads}">
+    <c:forEach var="ad" items="${ad}">
         <div class="col-md-6">
             <h2>${ad.title}</h2>
             <p>${ad.description}</p>
